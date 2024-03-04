9665565eed01e3afcd11e0b6a356fa3b8251756c
Drawsider <drawsider@gmail.com>
2024-03-04 18:01:40 +0300

    Refactor Roles.php to improve code structure and readability.

--- src/Roles.php
+++ src/Roles.php
@@ -3,25 +3,40 @@
 namespace Shigabutdinoff\Lararoles;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Shigabutdinoff\JsonRelation\JsonRelation;
 use Shigabutdinoff\Lararoles\Models\RoleModel;
 
 class Roles extends Controller
 {
-
+    protected $user;
     protected RoleModel $role;
 
     public function __construct()
     {
         $this->role = RoleModel::getModel();
+        $this->user = JsonRelation::addToModel(User::class)
+            ->hasOneMacro(RoleModel::class)
+            ->with($this->role->getTable());
     }
 
-    public static function index()
+    public function update($id, $roles)
     {
-        return (JsonRelation::addToModel(\App\Models\User::getModel())
-            ->hasOneMacro(Models\RoleModel::class, 'roles_clone')
-            ->with('roles_clone')
-            ->get()
-            );
+        $ok = $this->role->find($id)->update(['roles' => $roles]);
+        return response()->json([
+            'ok' => $ok,
+            'result' => $this->user->find($id),
+            'description' => '',
+        ]);
+    }
+
+    public function index()
+    {
+        $id = 2;
+        $roles = ['guest'];
+        return $this->update($id, $roles);
+
     }
 }
