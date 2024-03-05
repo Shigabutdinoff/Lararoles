9269ba4ed1b646ee8d0cf4f0ed0f63e53b9fef29
Drawsider <drawsider@gmail.com>
2024-03-05 16:46:29 +0300

    Update Roles.php methods to handle setting, unsetting, and updating user roles, providing clear descriptions for the actions taken.

--- src/Roles.php
+++ src/Roles.php
@@ -4,39 +4,98 @@ namespace Shigabutdinoff\Lararoles;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use Shigabutdinoff\JsonRelation\JsonRelation;
 use Shigabutdinoff\Lararoles\Models\RoleModel;
 
 class Roles extends Controller
 {
+    public string $table;
+
     protected $user;
-    protected RoleModel $role;
 
     public function __construct()
     {
-        $this->role = RoleModel::getModel();
+        $this->table = RoleModel::getModel()->getTable();
         $this->user = JsonRelation::addToModel(User::class)
             ->hasOneMacro(RoleModel::class)
-            ->with($this->role->getTable());
+            ->with($this->table);
+    }
+
+    /**
+     * Get the user Model use JsonRelation.
+     * @return mixed
+     */
+    public function user()
+    {
+        return $this->user->clone();
     }
 
     public function update($id, $roles)
     {
-        $ok = $this->role->find($id)->update(['roles' => $roles]);
+        $result = $this->user()->find((array)$id)->map(function ($model) use ($roles) {
+            $relation = $model->getRelation($this->table);
+            $updated = false;
+            if (! is_null($relation)) {
+                $updated = (bool) $relation->update(['roles' => ['guest', ... (array)$roles]]);
+            }
+            return [
+                'id' => $model->id,
+                'updated' => $updated
+            ];
+        });
+
+        $ok = $result->where('updated', true)->count() === count((array)$id);
+
         return response()->json([
             'ok' => $ok,
-            'result' => $this->user->find($id),
-            'description' => '',
+            'result' => $result,
+            'description' => 'The user\'s role has been ' . ($ok ? 'successfully' : 'unsuccessfully') . ' updated.',
         ]);
     }
 
-    public function index()
+    public function set($id, $roles)
     {
-        $id = 2;
-        $roles = ['guest'];
-        return $this->update($id, $roles);
+        $result = $this->user()->find((array)$id)->map(function ($model) use ($roles) {
+            $relation = $model->getRelation($this->table);
+            $pushed = false;
+            if (! is_null($relation)) {
+                $setted = (bool) $relation->update(['roles' => array_merge($relation->roles, (array)$roles)]);
+            }
+            return [
+                'id' => $model->id,
+                'setted' => $setted
+            ];
+        });
 
+        $ok = $result->where('setted', true)->count() === count((array)$id);
+
+        return response()->json([
+            'ok' => $ok,
+            'result' => $result,
+            'description' => 'The user\'s role has been ' . ($ok ? 'successfully' : 'unsuccessfully') . ' setted.',
+        ]);
+    }
+
+    public function unset($id, $roles)
+    {
+        $result = $this->user()->find((array)$id)->map(function ($model) use ($roles) {
+            $relation = $model->getRelation($this->table);
+            $unsetted = false;
+            if (! is_null($relation)) {
+                $unsetted = (bool) $relation->update(['roles' => ['guest', ... array_diff($relation->roles, (array)$roles)]]);
+            }
+            return [
+                'id' => $model->id,
+                'unsetted' => $unsetted
+            ];
+        });
+
+        $ok = $result->where('unsetted', true)->count() === count((array)$id);
+
+        return response()->json([
+            'ok' => $ok,
+            'result' => $result,
+            'description' => 'The user\'s role has been ' . ($ok ? 'successfully' : 'unsuccessfully') . ' unsetted.',
+        ]);
     }
 }
